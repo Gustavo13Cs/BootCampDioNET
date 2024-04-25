916cf8f3af6db1717dc8d9aa2468deafe44fcfa9
Gustavo Cunha Santos <105940671+Gustavo13Cs@users.noreply.github.com>
2024-04-24 23:46:22 -0300

    .

---
+++ Tuplas, Operador Ternário e Desconstrução de um Objeto com C#/Program.cs
@@ -0,0 +1,6 @@
+﻿(int,string,string,decimal) tupla = (1,"Gustavo","Cunha", 1.69M); 
+
+Console.WriteLine($"Id: {tupla.Item1}");
+Console.WriteLine($"Nome: {tupla.Item2}");
+Console.WriteLine($"Sobrenome: {tupla.Item3}");
+Console.WriteLine($"Altura: {tupla.Item4}");
