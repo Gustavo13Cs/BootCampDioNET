2f17abf656db46205c41964ab2a5f40fd609433d
Gustavo Cunha Santos <105940671+Gustavo13Cs@users.noreply.github.com>
2024-05-01 19:00:43 -0300

    .

---
+++ Configuração da Suíte de Testes e Aplicando Testes Integrados em .NET/Testes/ConsoleApp1/ConsoleApp1/Calculadora.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Calculadora
+    {
+
+        private List<string> listahistorico;
+
+        public Calculadora() { 
+            listahistorico = new List<string>();
+        }
+        public int somar (int num1, int num2)
+        {
+            int resultado = num1+ num2;
+
+            listahistorico.Insert(0,"Res: " + resultado);
+            return resultado;
+        }
+
+        public int sub(int num1, int num2)
+        {
+            int resultado = num1 - num2;
+            listahistorico.Insert(0, "Res: " + resultado);
+            return resultado;
+        }
+
+        public int multi(int num1, int num2)
+        {
+            int resultado = num1 * num2;
+            listahistorico.Insert(0, "Res: " + resultado);
+            return resultado;
+        }
+
+        public int divi(int num1, int num2)
+        {
+            int resultado = num1 / num2;
+            listahistorico.Insert(0, "Res: " + resultado);
+            return resultado;
+        }
+
+        public List<string> historico()
+        {
+
+            listahistorico.RemoveRange(3, listahistorico.Count - 3);
+            return listahistorico;
+        }
+    }
+}
